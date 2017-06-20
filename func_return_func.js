92523b1be4585289248f3bd0828f671351340af0
teja-vaitla <vaitla.teja@gmail.com>
2017-06-20 20:00:14 +0530

    continuing on generators

--- func_return_func.js
+++ func_return_func.js
@@ -80,7 +80,7 @@ let liftf = function(ops){
         @params{function}ops
         @params{Number}args1
         @params{Number}args2
-        @result {function} ops function with two params passed
+        @return {function} ops function with two params passed
     */
     return function(args1){
         return function(args2){
@@ -144,3 +144,87 @@ let composeu = function(ops1,ops2){
     };
 }
 console.log("\n Result of composeu function with (2) as parameter and (twice,square) as operators: "+ composeu(twice(add),twice(mul))(6)+'\n');
+
+let limit = function(ops,count){
+    /*
+        Takes a binary function to be called a limited number of times.
+         @params{function}ops
+         @params{Number}count
+         @params{Number}args1
+         @params{Number}args2
+         @return {function}  function with ops on params passed or undefined
+    */
+    return function(args1,args2){
+        if(count<=0){
+            return undefined;
+        }
+        else{
+            count-=1;
+            return ops(args1,args2);
+        }
+    };
+};
+
+let add_ltd = limit(add,3)
+console.log("add_ltd from limit function for count 2 with (2,3): "+add_ltd(2,3));
+console.log("add_ltd from limit function for count 2 with (2,3): "+add_ltd(2,3));
+console.log("add_ltd from limit function for count 2 with (2,3): "+add_ltd(2,3));
+console.log("add_ltd from limit function for count 2 with (2,3): "+add_ltd(2,3));
+
+
+let from = function(args){
+    /*
+    A generator function that produces a seies of values
+    @params{Number}args
+    @return {function} a generator function with series of values
+    */
+    return function(){
+        args+=1;
+        return args-1;
+    };
+}
+let index = from(2);
+console.log("Generator first execution starting from 0:  "+ index());
+console.log("Generator second execution starting from 0: "+ index());
+console.log("Generator third execution starting from 0:  "+ index());
+console.log("Generator fourth execution starting from 0: "+ index());
+
+let to = function(gen,count){
+    return function(){
+        if(count<=0){
+            return undefined;
+        }
+        else{
+            count-=1;
+            return gen();
+        }
+    };
+};
+let new_index = to(from(0),3);
+console.log("Generator first execution starting from 0:  "+ new_index());
+console.log("Generator second execution starting from 0: "+ new_index());
+console.log("Generator third execution starting from 0:  "+ new_index());
+console.log("Generator fourth execution starting from 0: "+ new_index());
+
+let fromto = function(start, end){
+    return to(from(start),end);
+};
+let newIndex = fromto(0,2);
+console.log("Generator first execution starting from 0:  "+ newIndex());
+console.log("Generator second execution starting from 0: "+ newIndex());
+console.log("Generator third execution starting from 0:  "+ newIndex());
+console.log("Generator fourth execution starting from 0: "+ newIndex());
+
+let ele = function(arr,gen){
+    return function(){
+        index = gen();
+        if(index<arr.length){
+            return arr[index];
+        }
+    }
+}
+let eleIndex = ele(['a','b','c','d'],fromto(1,3));
+console.log("Generator  execution on array  for[1,3]:  "+ eleIndex());
+console.log("Generator  execution on array  for[1,3]:  "+ eleIndex());
+console.log("Generator  execution on array  for[1,3]:  "+ eleIndex());
+console.log("Generator  execution on array  for[1,3]:  "+ eleIndex());
